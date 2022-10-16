d2f76bf5d3e20af982121d379bc6390885a72315
Yoga Baskoro <yoga.baskoro@ottencoffee.co.id>
2022-10-16 11:31:10 +0700

    update say hello func with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yogasab/go-say-hello
+module github.com/yogasab/go-say-hello/v2
 
 go 1.19
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello Updated"
+func SayHello(name string) string {
+	return "Hello from " + name
 }
